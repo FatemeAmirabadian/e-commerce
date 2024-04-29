326926fb9b2abb7ad07825e4bc0781d5f0e78d49
Fateme <fff.amirabadian@gmail.com>
2024-04-29 14:10:19 +0330

    fetchProductDatils

--- src/app/components/FetchProductDetails.js
+++ src/app/components/FetchProductDetails.js
@@ -8,11 +8,11 @@ async function fetchProductDetails(id) {
     if (!res.ok) {
       throw new Error("Failed to fetch product details");
     }
-    const product = await res.json();
-    return product;
+    const productData = await res.json();
+    return productData.data;
   } catch (error) {
     console.error("Error fetching product details:", error);
-    return null; // Return null if there's an error
+    return null;
   }
 }
 
